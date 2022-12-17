758b209504228e601051a3b27af0ffb1dc760b36
Yibin Z <zhyib002@outlook.com>
2022-12-17 19:57:39 +0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,6 @@ export default {
   port: 3000,
   // 数据库配置
   database: {
-    database: 'phantom',
-    username: 'ptm',
-    password: 'Txcld6118@',
-    host: '1.12.243.205',
-    port: 3306,
     dialectOptions: {
       requestTimeout: 90000,
     },
