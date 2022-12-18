3881ea0cd8ecb5f282b19f26d28a550bcac4b91b
Yibin Z <zhyib002@outlook.com>
2022-12-18 16:50:32 +0800

    new: frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import axiosUtil from './util/axiosRequest';
+onLaunch(() => {
+  console.log("App Launch");
+});
+onShow(() => {
+  console.log("App Show");
+  axiosUtil.get('/getAll')
+  .then((data) => {
+    console.log(data)
+  })
+});
+onHide(() => {
+  console.log("App Hide");
+});
+</script>
+<style></style>
---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    // 设置代理
+    proxy: {
+      '/api': {
+        target: 'http://1.12.243.205:3000/',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      },
+    }
+  },
+  plugins: [uni()],
+});
